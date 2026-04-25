bb658500b8e07d2dab71b095b6228533091304f7
steph-codex <stephkazuya@gmail.com>
2026-04-25 00:26:41 -0400

    Ajout interface OtakuStats

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import pandas as pd
-import matplotlib.pyplot as plt
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 data = []
 
@@ -16,24 +18,14 @@ class Otaku(BaseModel):
     engagement: str
     sleep: float
 
-
-@app.get("/plot")
-def plot():
-    df = pd.DataFrame(data)
-    plt.scatter(df["hours_anime"], df["sleep"])
-    plt.xlabel("Heures anime")
-    plt.ylabel("Sommeil")
-    plt.savefig("graph.png")
-    return {"message": "Graph généré"}
-@app.get("/")
-
-def home():
-    return {"message": "OtakuStats API fonctionne"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/submit")
 def submit(user: Otaku):
     data.append(user.dict())
-    return {"message": "Données enregistrées"}
+    return {"message": "OK"}
 
 @app.get("/stats")
 def stats():
@@ -43,8 +35,7 @@ def stats():
     df = pd.DataFrame(data)
 
     return {
-        "moyenne_heures_anime": df["hours_anime"].mean(),
-        "moyenne_manga": df["mangas_per_month"].mean(),
-        "moyenne_sommeil": df["sleep"].mean(),
+        "moyenne_heures": df["hours_anime"].mean(),
+        "moyenne_mangas": df["mangas_per_month"].mean(),
         "genre_populaire": df["genre"].mode()[0]
     }
